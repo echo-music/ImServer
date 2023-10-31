097c723a7d6cd67284025a453f358dfc5daef182
echo music <php_fangting@126.com>
2023-10-31 14:48:45 +0800

    图片上传修改地址

--- modules/file/service_minio.go
+++ modules/file/service_minio.go
@@ -53,7 +53,7 @@ func (sm *ServiceMinio) UploadFile(filePath string, contentType string, copyFile
 	secretAccessKey := minioConfig.SecretAccessKey
 	useSSL := false
 
-	endpoint = fmt.Sprintf("%s:%d", endpoint, 9000)
+	// endpoint = fmt.Sprintf("%s:%d", endpoint, 9000)
 	// if strings.HasPrefix(uploadUl.Scheme, "https") {
 	// 	useSSL = true
 	// }
